ac1c2cf7df1376c9668e2121a6d4ebd683b96c66
Impyy <alex@impy.me>
2016-08-16 14:14:17 +0200

    Reworked MainActivity to include an FA button and a settings button

--- app/src/main/java/me/impy/aegis/MainActivity.java
+++ app/src/main/java/me/impy/aegis/MainActivity.java
@@ -1,10 +1,13 @@
 package me.impy.aegis;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import me.impy.aegis.crypto.KeyInfo;
@@ -13,22 +16,25 @@ import me.impy.aegis.crypto.OTP;
 public class MainActivity extends AppCompatActivity {
 
     static final int GET_KEYINFO = 1;
-    Button btnScan;
     TextView tvTotp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        btnScan = (Button) findViewById(R.id.button);
-        tvTotp = (TextView) findViewById(R.id.textView2);
-        btnScan.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent scannerActivity = new Intent(getApplicationContext(), ScannerActivity.class);
                 startActivityForResult(scannerActivity, GET_KEYINFO);
             }
         });
+
+        tvTotp = (TextView) findViewById(R.id.textView2);
     }
 
     @Override
@@ -49,5 +55,27 @@ public class MainActivity extends AppCompatActivity {
                 tvTotp.setText(otp);
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
