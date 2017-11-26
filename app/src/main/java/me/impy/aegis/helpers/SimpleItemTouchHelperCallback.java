74536dbda69945e6c80a6b431f25dfe37e38dffc
Alexander Bakker <ab@alexbakker.me>
2017-11-26 21:34:54 +0100

    Don't allow swiping in the key profile list

--- app/src/main/java/me/impy/aegis/helpers/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/me/impy/aegis/helpers/SimpleItemTouchHelperCallback.java
@@ -5,39 +5,38 @@ import android.support.v7.widget.helper.ItemTouchHelper;
 
 public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
 
-        private final ItemTouchHelperAdapter _adapter;
+    private final ItemTouchHelperAdapter _adapter;
 
-        public SimpleItemTouchHelperCallback(ItemTouchHelperAdapter adapter) {
-            _adapter = adapter;
-        }
-
-        @Override
-        public boolean isLongPressDragEnabled() {
-            return true;
-        }
-
-        @Override
-        public boolean isItemViewSwipeEnabled() {
-            return true;
-        }
-
-        @Override
-        public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
-            int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
-            int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
-            return makeMovementFlags(dragFlags, swipeFlags);
-        }
-
-        @Override
-        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
-                              RecyclerView.ViewHolder target) {
-            _adapter.onItemMove(viewHolder.getAdapterPosition(), target.getAdapterPosition());
-            return true;
-        }
-
-        @Override
-        public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-            _adapter.onItemDismiss(viewHolder.getAdapterPosition());
-        }
-
+    public SimpleItemTouchHelperCallback(ItemTouchHelperAdapter adapter) {
+        _adapter = adapter;
     }
+
+    @Override
+    public boolean isLongPressDragEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isItemViewSwipeEnabled() {
+        return false;
+    }
+
+    @Override
+    public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+        int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
+        return makeMovementFlags(dragFlags, swipeFlags);
+    }
+
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                          RecyclerView.ViewHolder target) {
+        _adapter.onItemMove(viewHolder.getAdapterPosition(), target.getAdapterPosition());
+        return true;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+        _adapter.onItemDismiss(viewHolder.getAdapterPosition());
+    }
+}
