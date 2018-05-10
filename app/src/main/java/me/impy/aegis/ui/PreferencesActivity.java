7630bbfe252073f4124840b15ffb564d4750bf66
Alexander Bakker <ab@alexbakker.me>
2018-05-10 21:20:16 +0200

    Make sure permission request results are passed down to PreferencesFragment

--- app/src/main/java/me/impy/aegis/ui/PreferencesActivity.java
+++ app/src/main/java/me/impy/aegis/ui/PreferencesActivity.java
@@ -5,13 +5,15 @@ import android.os.Bundle;
 import me.impy.aegis.R;
 
 public class PreferencesActivity extends AegisActivity {
+    private PreferencesFragment _fragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        PreferencesFragment fragment = new PreferencesFragment();
-        fragment.setArguments(getIntent().getExtras());
-        getFragmentManager().beginTransaction().replace(android.R.id.content, fragment).commit();
+        _fragment = new PreferencesFragment();
+        _fragment.setArguments(getIntent().getExtras());
+        getFragmentManager().beginTransaction().replace(android.R.id.content, _fragment).commit();
     }
 
     @Override
@@ -21,5 +23,11 @@ public class PreferencesActivity extends AegisActivity {
         } else {
             setTheme(R.style.AppTheme_Default);
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // pass permission request results to the fragment
+        _fragment.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
