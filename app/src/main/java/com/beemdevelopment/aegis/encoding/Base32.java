b81ec0f07310f8d0401d744ebd6b3fce18dd2388
Alexander Bakker <ab@alexbakker.me>
2020-06-05 12:49:12 +0200

    Fix importing andOTP backups with more than 10000 PBKDF iterations
    These larger numbers of iterations cause the key derivation process to take
    longer, so I also moved that task to the background. andOTP now also has a
    proper "issuer" field, so we make use of that as well.
    
    Also fixes an issue where padded base32 could not be decoded. This issue is only
    present for the andOTP importer as far as I know, so that's why that change is
    included here.

--- app/src/main/java/com/beemdevelopment/aegis/encoding/Base32.java
+++ app/src/main/java/com/beemdevelopment/aegis/encoding/Base32.java
@@ -3,21 +3,19 @@ package com.beemdevelopment.aegis.encoding;
 import com.google.common.io.BaseEncoding;
 
 public class Base32 {
-    private static final BaseEncoding _encoding = BaseEncoding.base32().omitPadding();
-
     private Base32() {
 
     }
 
     public static byte[] decode(String s) throws EncodingException {
         try {
-            return _encoding.decode(s.toUpperCase());
+            return BaseEncoding.base32().decode(s.toUpperCase());
         } catch (IllegalArgumentException e) {
             throw new EncodingException(e);
         }
     }
 
     public static String encode(byte[] data) {
-        return _encoding.encode(data);
+        return BaseEncoding.base32().omitPadding().encode(data);
     }
 }
