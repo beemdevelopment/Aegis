beemdevelopment/Aegis

commit 93cc945a2c9cd1cdaefc745b33a196cdeec198ac

Fix division by zero when calculating progress for large periods

app/src/main/java/com/beemdevelopment/aegis/ui/views/TotpProgressBar.java
@@ -65,8 +65,8 @@ public class TotpProgressBar extends ProgressBar {
         int currentProgress = (int) (maxProgress * ((float) millisTillRotation / (_period * 1000)));
 
         // start progress animation, compensating for any changes to the animator duration scale settings
-        int animPart = maxProgress / _period;
-        int animEnd = (currentProgress / animPart) * animPart;
+        float animPart = (float) maxProgress / _period;
+        int animEnd = (int) ((currentProgress / animPart) * animPart);
         int animPartDuration = (int) (1000 / _animDurationScale);
         float animDurationFraction = (float) (currentProgress - animEnd) / animPart;
         int realAnimDuration =  (int) (1000 * animDurationFraction);

app/src/main/res/layout/card_entry_compact.xml
@@ -156,7 +156,7 @@
             android:layout_width="wrap_content"
             android:layout_height="3dp"
             android:id="@+id/progressBar"
-            android:max="1000"
+            android:max="5000"
             android:layout_weight="1"/>
     </LinearLayout>
 

app/src/main/res/layout/card_entry_small.xml
@@ -155,7 +155,7 @@
             android:layout_width="wrap_content"
             android:layout_height="3dp"
             android:id="@+id/progressBar"
-            android:max="1000"
+            android:max="5000"
             android:layout_weight="1"/>
     </LinearLayout>
 </LinearLayout>

commit de3d0d85152e668bd5724e6202bf728dcba2a226

Fix an issue where the progress bar refresher ran in a tight loop
If the animator scale setting is set to 0, the progress bar refresher would run
in a tight loop. This issue was introduced by
93cc945a2c9cd1cdaefc745b33a196cdeec198ac and caused the UI tests to fail.

app/src/main/java/com/beemdevelopment/aegis/ui/views/TotpProgressBar.java
@@ -66,11 +66,12 @@ public class TotpProgressBar extends ProgressBar {
 
         // start progress animation, compensating for any changes to the animator duration scale settings
         float animPart = (float) maxProgress / _period;
-        int animEnd = (int) ((currentProgress / animPart) * animPart);
-        int animPartDuration = (int) (1000 / _animDurationScale);
+        int animEnd = (int) (Math.floor(currentProgress / animPart) * animPart);
+        int animPartDuration = _animDurationScale > 0 ? (int) (1000 / _animDurationScale) : 0;
         float animDurationFraction = (float) (currentProgress - animEnd) / animPart;
-        int realAnimDuration =  (int) (1000 * animDurationFraction);
+        int realAnimDuration = (int) (1000 * animDurationFraction);
         int animDuration =  (int) (animPartDuration * animDurationFraction);
+
         ObjectAnimator animation = ObjectAnimator.ofInt(this, "progress", currentProgress, animEnd);
         animation.setDuration(animDuration);
         animation.setInterpolator(new LinearInterpolator());