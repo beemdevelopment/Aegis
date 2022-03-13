bece74a2d6777883b515ac22ad71c17f690b56bc
judy <judy8889107@gmail.com>
2022-03-13 12:08:00 +0800

    judy 新增UrlCheck函數 *可以利用URLConnection去抓取.html內容

--- app/src/main/java/com/beemdevelopment/aegis/ui/views/UrlCheckActivity.java
+++ app/src/main/java/com/beemdevelopment/aegis/ui/views/UrlCheckActivity.java
@@ -10,8 +10,14 @@ import android.widget.Button;
 import android.widget.EditText;
 /* 控制鍵盤 */
 import android.view.inputmethod.InputMethodManager;
+/* ImageButton的import */
 import android.widget.ImageButton;
 
+/* URL lib */
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 import com.beemdevelopment.aegis.R;
 
@@ -47,6 +53,7 @@ public class UrlCheckActivity extends AegisActivity {
 
 
 
+
         /* 監聽器設定 */
         send_button.setOnClickListener(new View.OnClickListener()  {
             @Override
@@ -55,6 +62,8 @@ public class UrlCheckActivity extends AegisActivity {
                 /* 按下send_button就隱藏鍵盤 */
                 InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(send_button.getWindowToken(), 0);
+                UrlCheck(url_input.getText().toString());
+
 
             }
         });
@@ -70,5 +79,42 @@ public class UrlCheckActivity extends AegisActivity {
 
 
     }
+    /* 檢查URL function */
+    public void UrlCheck(String url_text){
+        /* URL parameter */
+        URL url;
+        URLConnection urlConnection; /* 實例可用於讀取和寫入此 URL 引用的資源，可以得到 .html 相關資訊 */
+        try{
+            /* 設定變數 */
+            url = new URL(url_text);
+            urlConnection = url.openConnection();
+            /* print參數 */
+            System.out.println("URL參數：");
+            System.out.println("URL：" + url.toString());
+            System.out.println("協議：" + url.getProtocol());
+            System.out.println("驗證信息：" + url.getAuthority());
+            System.out.println("文件名及請求參數：" + url.getFile());
+            System.out.println("主機名：" + url.getHost());
+            System.out.println("路徑：" + url.getPath());
+            System.out.println("端口：" + url.getPort());
+            System.out.println("默認端口：" + url.getDefaultPort());
+            System.out.println("請求參數：" + url.getQuery());
+            System.out.println("定位位置：" + url.getRef());
+            /* print UrlConnection */
+            System.out.println("");
+            System.out.println("URL Connection參數: ");
+//            System.out.println(urlConnection.getContentType());
+//            System.out.println(urlConnection.getContentLength());
+//            System.out.println(urlConnection.getContentEncoding());
+//            System.out.println(urlConnection.getDate());
+//            System.out.println(urlConnection.getExpiration());
+//            System.out.println(urlConnection.getLastModified());
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
 }
 
