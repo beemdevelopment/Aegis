beemdevelopment/Aegis

commit ffcbaffcfc04d27d59e01bbc4bf432bda0d9eec5

Sort list after editing entry

app/src/main/java/com/beemdevelopment/aegis/ui/views/EntryAdapter.java
@@ -192,15 +192,21 @@ public class EntryAdapter extends RecyclerView.Adapter<EntryHolder> implements I
             } else {
                 _shownEntries.set(position, newEntry);
                 notifyItemChanged(position);
+                if (_sortCategory != null) {
+                    Comparator<VaultEntry> comparator = _sortCategory.getComparator();
+                    Collections.sort(_shownEntries, comparator);
+                    int newPosition = Collections.binarySearch(_shownEntries, newEntry, comparator);
+                    if (position != newPosition) {
+                        notifyItemMoved(position, newPosition);
+                    }
+                }
             }
         } else if (!isEntryFiltered(newEntry)) {
-            // TODO: preserve order
             _shownEntries.add(newEntry);
 
             int position = getItemCount() - 1;
             notifyItemInserted(position);
         }
-
         checkPeriodUniformity();
     }
 

commit c27d080a1102e0508b24214de5969c52d7e3dbd1

Fix call to notifyItemMoved when entry change causes sort change
This fixes an issue introduced in ffcbaffcfc04d27d59e01bbc4bf432bda0d9eec5

app/src/main/java/com/beemdevelopment/aegis/ui/views/EntryAdapter.java
@@ -223,7 +223,7 @@ public class EntryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
                     Comparator<VaultEntry> comparator = _sortCategory.getComparator();
                     if (comparator != null) {
                         Collections.sort(_shownEntries, comparator);
-                        int newPosition = Collections.binarySearch(_shownEntries, newEntry, comparator);
+                        int newPosition = _shownEntries.indexOf(newEntry);
                         if (position != newPosition) {
                             notifyItemMoved(position, newPosition);
                         }