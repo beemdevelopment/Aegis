beemdevelopment/Aegis

commit 46e1421c28857034c135f3c3facbe8b9b5c6483d

Start auth/intro activities from onStart to work around an Android bug

app/src/main/java/com/beemdevelopment/aegis/ui/MainActivity.java
@@ -657,8 +657,8 @@ public class MainActivity extends AegisActivity implements EntryListView.Listene
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onStart() {
+        super.onStart();
 
         if (_vaultManager.isVaultInitNeeded()) {
             if (_prefs.isIntroDone()) {

commit f7862dcdf030704107ac81653b16a2205b698cf5

Initialize the group chip properly after unlocking Aegis
This fixes an issue introduced by
46e1421c28857034c135f3c3facbe8b9b5c6483d where the group chip would not
show after unlocking Aegis. This happened because the activity result is
received *after* ``onStart``. When we were using ``onResume``, it was
the other way around.

app/src/main/java/com/beemdevelopment/aegis/ui/MainActivity.java
@@ -857,6 +857,7 @@ public class MainActivity extends AegisActivity implements EntryListView.Listene
 
     private void loadEntries() {
         if (!_loaded) {
+            _entryListView.setGroups(_vaultManager.getVault().getUsedGroups());
             _entryListView.setUsageCounts(_prefs.getUsageCounts());
             _entryListView.setLastUsedTimestamps(_prefs.getLastUsedTimestamps());
             _entryListView.addEntries(_vaultManager.getVault().getEntries());