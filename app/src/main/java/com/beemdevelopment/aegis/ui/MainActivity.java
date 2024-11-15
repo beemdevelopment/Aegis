beemdevelopment/Aegis

commit 46e1421c28857034c135f3c3facbe8b9b5c6483d

Start auth/intro activities from onStart to work around an Android bug

app/src/main/java/com/beemdevelopment/aegis/ui/MainActivity.java
@@ -657,8 +657,8 @@ public class MainActivity extends AegisActivity implements EntryListView.Listene
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onStart() {
+        super.onStart();
 
         if (_vaultManager.isVaultInitNeeded()) {
             if (_prefs.isIntroDone()) {

commit 9d383b85d8c5581f12a1a22407ce4615c4d499f3

Update sort category radio button state in the menu
This has been broken since 46e1421c28857034c135f3c3facbe8b9b5c6483d.

I also removed some other logic from ``onCreateOptionsMenu`` that
doesn't seem to belong there anymore.

app/src/main/java/com/beemdevelopment/aegis/ui/MainActivity.java
@@ -694,8 +694,10 @@ public class MainActivity extends AegisActivity implements EntryListView.Listene
     }
 
     private void updateSortCategoryMenu() {
-        SortCategory category = _prefs.getCurrentSortCategory();
-        _menu.findItem(category.getMenuItem()).setChecked(true);
+        if (_menu != null) {
+            SortCategory category = _prefs.getCurrentSortCategory();
+            _menu.findItem(category.getMenuItem()).setChecked(true);
+        }
     }
 
     private void onIntroResult() {
@@ -908,6 +910,7 @@ public class MainActivity extends AegisActivity implements EntryListView.Listene
 
         handleIncomingIntent();
         updateLockIcon();
+        updateSortCategoryMenu();
         doShortcutActions();
         updateErrorCard();
     }
@@ -917,14 +920,7 @@ public class MainActivity extends AegisActivity implements EntryListView.Listene
         _menu = menu;
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
-        updateLockIcon();
-        if (_loaded) {
-            setGroups(_vaultManager.getVault().getUsedGroups());
-            updateSortCategoryMenu();
-        }
-
         MenuItem searchViewMenuItem = menu.findItem(R.id.mi_search);
-
         _searchView = (SearchView) searchViewMenuItem.getActionView();
         _searchView.setMaxWidth(Integer.MAX_VALUE);
         _searchView.setOnQueryTextFocusChangeListener((v, hasFocus) -> {