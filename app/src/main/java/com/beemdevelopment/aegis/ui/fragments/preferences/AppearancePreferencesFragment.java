53676dfe6f28189e9e6977124c460bf4db321e1a
HrshD1eux <157795651+HrshD1eux@users.noreply.github.com>
2026-07-28 16:32:51 +0530

    feat: Add dynamic custom app icons with adaptive icon support

--- app/src/main/java/com/beemdevelopment/aegis/ui/fragments/preferences/AppearancePreferencesFragment.java
+++ app/src/main/java/com/beemdevelopment/aegis/ui/fragments/preferences/AppearancePreferencesFragment.java
@@ -7,6 +7,19 @@ import android.os.Bundle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.preference.Preference;
 
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.beemdevelopment.aegis.AccountNamePosition;
 import com.beemdevelopment.aegis.Preferences;
 import com.beemdevelopment.aegis.R;
@@ -66,6 +79,97 @@ public class AppearancePreferencesFragment extends PreferencesFragment {
                     .setNegativeButton(android.R.string.cancel, null)
                     .create());
 
+            return true;
+        });
+
+        Preference appIconPreference = requirePreference("pref_app_icon");
+        String[] appIconTitles = getResources().getStringArray(R.array.app_icon_titles);
+        String[] appIconValues = getResources().getStringArray(R.array.app_icon_values);
+        int currentAppIconIndex = Arrays.asList(appIconValues).indexOf(_prefs.getAppIcon());
+        if (currentAppIconIndex == -1) currentAppIconIndex = 0;
+        appIconPreference.setSummary(String.format("%s: %s", getString(R.string.selected), appIconTitles[currentAppIconIndex]));
+        
+        appIconPreference.setOnPreferenceClickListener(preference -> {
+            View dialogView = LayoutInflater.from(requireContext()).inflate(R.layout.dialog_app_icon_picker, null);
+            RecyclerView recyclerView = dialogView.findViewById(R.id.app_icon_recycler);
+            recyclerView.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
+
+            AlertDialog dialog = new MaterialAlertDialogBuilder(requireContext())
+                    .setTitle(R.string.pref_app_icon_title)
+                    .setView(dialogView)
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .create();
+
+            recyclerView.setAdapter(new RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+                @NonNull
+                @Override
+                public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                    View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_app_icon, parent, false);
+                    return new RecyclerView.ViewHolder(view) {};
+                }
+
+                @Override
+                public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+                    String selectedIconValue = appIconValues[position];
+                    String selectedIconTitle = appIconTitles[position];
+                    ImageView imageView = holder.itemView.findViewById(R.id.app_icon_image);
+                    TextView textView = holder.itemView.findViewById(R.id.app_icon_name);
+                    View borderView = holder.itemView.findViewById(R.id.app_icon_border);
+
+                    textView.setText(selectedIconTitle);
+                    if (selectedIconValue.equals(_prefs.getAppIcon())) {
+                        borderView.setVisibility(View.VISIBLE);
+                    } else {
+                        borderView.setVisibility(View.GONE);
+                    }
+                    
+                    int resId = R.mipmap.ic_launcher;
+                    if (!selectedIconValue.equals("Default")) {
+                        String resName = "ic_launcher_" + selectedIconValue.replaceAll("([a-z])([A-Z]+)", "$1_$2").toLowerCase();
+                        int dynamicResId = getResources().getIdentifier(resName, "mipmap", requireContext().getPackageName());
+                        if (dynamicResId != 0) resId = dynamicResId;
+                    }
+                    imageView.setImageResource(resId);
+                    
+                    holder.itemView.setOnClickListener(v -> {
+                        dialog.dismiss();
+                        if (!selectedIconValue.equals(_prefs.getAppIcon())) {
+                            Dialogs.showSecureDialog(new MaterialAlertDialogBuilder(requireContext())
+                                    .setTitle(R.string.pref_app_icon_title)
+                                    .setMessage(R.string.pref_app_icon_warning)
+                                    .setPositiveButton(android.R.string.ok, (dialog2, which2) -> {
+                                        _prefs.setAppIcon(selectedIconValue);
+                                        
+                                        PackageManager pm = requireContext().getPackageManager();
+                                        String pkg = requireContext().getPackageName();
+                                        
+                                        // Disable old ones with DONT_KILL_APP
+                                        for (String val : appIconValues) {
+                                            if (!val.equals(selectedIconValue)) {
+                                                ComponentName comp = new ComponentName(pkg, "com.beemdevelopment.aegis.Alias" + val);
+                                                pm.setComponentEnabledSetting(comp, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+                                            }
+                                        }
+                                        
+                                        // Enable the new one without DONT_KILL_APP so Android forces the launcher refresh and kills the app
+                                        ComponentName comp = new ComponentName(pkg, "com.beemdevelopment.aegis.Alias" + selectedIconValue);
+                                        pm.setComponentEnabledSetting(comp, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, 0);
+                                        
+                                        dialog2.dismiss();
+                                    })
+                                    .setNegativeButton(android.R.string.cancel, null)
+                                    .create());
+                        }
+                    });
+                }
+
+                @Override
+                public int getItemCount() {
+                    return appIconValues.length;
+                }
+            });
+
+            Dialogs.showSecureDialog(dialog);
             return true;
         });
 
